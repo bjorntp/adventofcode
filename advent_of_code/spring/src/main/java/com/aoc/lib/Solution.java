b18d1a33feed7e990741b8305c113eda9cfdba62
Björn Tenje Persson <bjorn@tenje.se>
2024-12-01 20:38:58 +0100

    Init
    Moved to its' own repo.

---
+++ advent_of_code/spring/src/main/java/com/aoc/lib/Solution.java
@@ -0,0 +1,15 @@
+package com.aoc.lib;
+
+public abstract class Solution {
+
+  protected String input;
+
+  public Solution(String input) {
+    this.input = input;
+  }
+
+  public abstract String task_1();
+
+  public abstract String task_2();
+
+}
