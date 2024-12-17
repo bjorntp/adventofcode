5e16793e9a6d2e9bb9fbda9d5965ed287e6a5b43
Björn Tenje Persson <bjorn@tenje.se>
2024-12-17 14:07:27 +0100

    Init all days

--- spring/src/main/java/com/aoc/solutions_2024/S_15.java
+++ spring/src/main/java/com/aoc/solutions_2024/S_15.java
@@ -26,7 +26,7 @@ public class S_15 extends Solution {
         }
       }
     }
-    ObjectMover ob = new ObjectMover(mapGrid, x, y);
+    ObjectMoverTask1 ob = new ObjectMoverTask1(mapGrid, x, y);
     for (int i = 0; i < sequence.length(); i++) {
       ob.Move(sequence.charAt(i), x, y, true);
       x = ob.getRobotX();
@@ -41,24 +41,196 @@ public class S_15 extends Solution {
         }
       }
     }
-
     return "" + total;
   }
 
   @Override
   public String task_2() {
+    String map = input.split("\n\n")[0];
+    String sequence = input.split("\n\n")[1];
+    InputHandler ih = new InputHandler(map);
+    Character[][] firstMapGrid = ih.getMatrix();
+    Character[][] finalGrid = new Character[firstMapGrid.length][firstMapGrid[0].length * 2];
+    int x = -1;
+    int y = -1;
+    for (int i = 0; i < firstMapGrid.length; i++) {
+      for (int j = 0; j < firstMapGrid[i].length; j++) {
+        switch (firstMapGrid[i][j]) {
+          case '.':
+            finalGrid[i][2 * j] = '.';
+            finalGrid[i][2 * j + 1] = '.';
+            break;
+          case '#':
+            finalGrid[i][2 * j] = '#';
+            finalGrid[i][2 * j + 1] = '#';
+            break;
+          case 'O':
+            finalGrid[i][2 * j] = '[';
+            finalGrid[i][2 * j + 1] = ']';
+            break;
+          default:
+            finalGrid[i][2 * j] = '@';
+            finalGrid[i][2 * j + 1] = '.';
+            break;
+        }
+      }
+    }
+    System.out.println("--------");
+    for (int i = 0; i < finalGrid.length; i++) {
+      for (int j = 0; j < finalGrid[i].length; j++) {
+        System.out.print(finalGrid[i][j]);
+        if (finalGrid[i][j] == '@') {
+          x = j;
+          y = i;
+        }
+      }
+      System.out.println("");
+    }
+    ObjectMoverTask2 ob = new ObjectMoverTask2(finalGrid, x, y);
+    for (int i = 0; i < sequence.length(); i++) {
+      ob.Move(sequence.charAt(i), x, y, true);
+      x = ob.getRobotX();
+      y = ob.getRobotY();
+      System.out.println("Direction: " + sequence.charAt(i));
+      ob.printMap();
+    }
+    int total = 0;
+    // for (int i = 0; i < mapGrid.length; i++) {
+    // for (int j = 0; j < mapGrid[i].length; j++) {
+    // if (mapGrid[i][j] == 'O') {
+    // total += i * 100 + j;
+    // }
+    // }
+    // }
     return "";
+  }
+
+  public class ObjectMoverTask2 {
+    Character[][] map;
+    int robotX, robotY;
+
+    public ObjectMoverTask2(Character[][] map, int robotX, int robotY) {
+      this.map = map;
+      this.robotX = robotX;
+      this.robotY = robotY;
+    }
+
+    public boolean Move(char direction, int x, int y, boolean robot) {
+      boolean moved = true;
+      if (direction == '<') {
+        if (map[y][x - 1] == '#') {
+          return false;
+        }
+        if (map[y][x - 1] == '[' || map[y][x - 1] == ']') {
+          moved = Move(direction, x - 1, y, false);
+        }
+        if (map[y][x - 1] == '.') {
+          map[y][x - 1] = map[y][x];
+          map[y][x] = '.';
+          if (robot && moved) {
+            robotX--;
+          }
+        }
+
+      } else if (direction == '>') {
+        if (map[y][x + 1] == '#') {
+          return false;
+        }
+        if (map[y][x + 1] == '[' || map[y][x + 1] == ']') {
+          moved = Move(direction, x + 1, y, false);
+        }
+        if (map[y][x + 1] == '.') {
+          map[y][x + 1] = map[y][x];
+          map[y][x] = '.';
+          if (robot && moved) {
+            robotX++;
+          }
+        }
+
+      } else if (direction == '^') {
+        if (map[y - 1][x] == '#') {
+          return false;
+        }
+        if (map[y - 1][x] == '[') {
+          moved = Move(direction, x, y - 1, false);
+          if (moved) {
+            moved = Move(direction, x + 1, y, false);
+          }
+        } else if (map[y - 1][x] == ']') {
+          moved = Move(direction, x, y - 1, false);
+          if (moved) {
+            Move(direction, x - 1, y, false);
+          }
+        }
+        if (map[y - 1][x] == '.') {
+          map[y - 1][x] = map[y][x];
+          map[y][x] = '.';
+          if (robot && moved) {
+            robotY--;
+          }
+        }
+
+      } else if (direction == 'v') {
+        if (map[y + 1][x] == '#') {
+          return false;
+        }
+        if (map[y + 1][x] == '[') {
+          moved = Move(direction, x, y + 1, false);
+          if (moved) {
+            Move(direction, x + 1, y, false);
+          }
+        } else if (map[y + 1][x] == ']') {
+          moved = Move(direction, x, y + 1, false);
+          if (moved) {
+            Move(direction, x - 1, y, false);
+          }
+        }
+        if (map[y + 1][x] == '.') {
+          map[y + 1][x] = map[y][x];
+          map[y][x] = '.';
+          if (robot && moved) {
+            robotY++;
+          }
+        }
+      }
+      return moved;
+    }
+
+    public Character[][] getMap() {
+      return map;
+    }
+
+    public int getRobotX() {
+      return robotX;
+    }
+
+    public int getRobotY() {
+      return robotY;
+    }
+
+    public void printMap() {
+      for (int i = 0; i < map.length; i++) {
+        for (int j = 0; j < map[i].length; j++) {
+          System.out.print(map[i][j]);
+        }
+        System.out.println("");
+      }
+      System.out.println("");
+      System.out.println("-------");
+      System.out.println("");
+    }
+
   }
 
   /**
    * ObjectMover
    */
-  public class ObjectMover {
+  public class ObjectMoverTask1 {
 
     Character[][] map;
     int robotX, robotY;
 
-    public ObjectMover(Character[][] map, int robotX, int robotY) {
+    public ObjectMoverTask1(Character[][] map, int robotX, int robotY) {
       this.map = map;
       this.robotX = robotX;
       this.robotY = robotY;
---
+++ spring/src/main/java/com/aoc/solutions_2024/S_17.java
@@ -0,0 +1,25 @@
+package com.aoc.solutions_2024;
+
+import com.aoc.lib.*;
+
+public class S_17 extends Solution {
+
+  private InputHandler ih;
+
+  public S_17(String input) {
+    super(input);
+    ih = new InputHandler(input);
+  }
+
+  @Override
+  public String task_1() {
+    Character[][] grid = ih.getMatrix();
+
+    return "";
+  }
+
+  @Override
+  public String task_2() {
+    return "";
+  }
+}
